6836863c7335812ff2a2c2cd6da4a100cf730421
Loner1536 <Williamdleady4@gmail.com>
2026-02-21 20:36:25 +0000

    removed some unused packages

--- test/src/stories/test/test.story.tsx
+++ test/src/stories/test/test.story.tsx
@@ -1,33 +1,15 @@
 // Package
-import { Boolean, ControlGroup, CreateVideStory, type InferVideProps } from "@rbxts/ui-labs";
+import { CreateVideStory, type InferVideProps } from "@rbxts/ui-labs";
 import Vide from "@rbxts/vide";
 
-// Dependencies
-import Setup from "../setup";
-
-const controls = {
-	Template: ControlGroup({
-		visible: Boolean(true),
-	}),
-	Child: ControlGroup({
-		visible: Boolean(true),
-	}),
-};
+const controls = {};
 
 const story = CreateVideStory(
 	{
 		vide: Vide,
 		controls,
 	},
-	(storyProps: InferVideProps<typeof controls>) => (
-		<Setup
-			storyProps={storyProps}
-			callback={(_, forge) => {
-				forge.bind("Template", undefined, storyProps.controls.Template.visible);
-				forge.bind("Child", undefined, storyProps.controls.Child.visible);
-			}}
-		/>
-	),
+	(props: InferVideProps<typeof controls>) => {},
 );
 
 export = story;
